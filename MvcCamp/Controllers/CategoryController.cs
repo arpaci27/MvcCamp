2e0462be8d9d0d02287c1ced1de500ab8a8e5994
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-18 23:03:41 +0300

    Getting categories list from database

--- MvcCamp/Controllers/CategoryController.cs
+++ MvcCamp/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcCamp.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBl(); 
+            return View(categoryValues);
         }
     }
 }
