e5182a3831b2e4a5cc9c46bf8a1694e3f4c58693
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-19 21:51:52 +0300

    Adding new category, Listing categories has added inside to Admin Layout.

--- MvcCamp/Controllers/AdminCategoryController.cs
+++ MvcCamp/Controllers/AdminCategoryController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccesLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcCamp.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal(new Context()));
+
         public IActionResult Index()
         {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            CategoryValidation categoryValidation = new CategoryValidation();
+            ValidationResult results = categoryValidation.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index", "AdminCategory"); // Bu satırın AdminCategory'ye yönlendiğinden emin olun
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(); // Validasyon hataları ile AddCategory görünümünü geri döndür
+            }
         }
     }
 }
--- MvcCamp/Controllers/CategoryController.cs
+++ MvcCamp/Controllers/CategoryController.cs
@@ -1,55 +1,54 @@
-﻿using BusinessLayer.Concrete;
-using BusinessLayer.ValidationRules;
-using DataAccesLayer.Concrete;
-using DataAccesLayer.EntityFramework;
-using EntityLayer.Concrete;
-using FluentValidation.Results;
-using Microsoft.AspNetCore.Mvc;
+﻿//using BusinessLayer.Concrete;
+//using BusinessLayer.ValidationRules;
+//using DataAccesLayer.Concrete;
+//using DataAccesLayer.EntityFramework;
+//using EntityLayer.Concrete;
+//using FluentValidation.Results;
+//using Microsoft.AspNetCore.Mvc;
 
 
-namespace MvcCamp.Controllers
-{
-    public class CategoryController : Controller
-    {
-        CategoryManager cm = new CategoryManager(new EfCategoryDal(new Context()));
-        public IActionResult Index()
-        {
-            return View();
-        }
+//namespace MvcCamp.Controllers
+//{
+//    public class CategoryController : Controller
+//    {
+//        CategoryManager cm = new CategoryManager(new EfCategoryDal(new Context()));
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
 
-        public IActionResult GetCategoryList()
-        {
-            var categoryValues = cm.GetList(); 
-            return View(categoryValues);
-        }
-        [HttpGet]
+//        public IActionResult GetCategoryList()
+//        {
+//            var categoryValues = cm.GetList(); 
+//            return View(categoryValues);
+//        }
+//        [HttpGet]
 
 
-        public IActionResult AddCategory()
-        {
-            return View();
-        }
+//        public IActionResult AddCategory()
+//        {
+//            return View();
+//        }
 
-        [HttpPost]
-        public IActionResult AddCategory(Category p)
-        {
-            CategoryValidation categoryValidation = new CategoryValidation();
-            ValidationResult results = categoryValidation.Validate(p);
-            if (results.IsValid)
-            {
-                cm.CategoryAdd(p);
-                Console.WriteLine("Category Added");
+//        [HttpPost]
+//        public IActionResult AddCategory(Category p)
+//        {
+//            CategoryValidation categoryValidation = new CategoryValidation();
+//            ValidationResult results = categoryValidation.Validate(p);
+//            if (results.IsValid)
+//            {
+//                cm.CategoryAdd(p);
 
-                return RedirectToAction("GetCategoryList");
-            }
-            else
-            {
-                foreach (var item in results.Errors)
-                {
-                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-                }
-            }
-            return View();
-        }
-    }
-}
+//                return RedirectToAction("GetCategoryList");
+//            }
+//            else
+//            {
+//                foreach (var item in results.Errors)
+//                {
+//                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+//                }
+//            }
+//            return View();
+//        }
+//    }
+//}
