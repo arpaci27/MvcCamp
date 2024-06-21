7687bf9cd4df67ddaa9416f3938e5ad5f63f1cb4
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-21 16:05:36 +0300

    Heading add system modified, Category and Writer can be selected on "Dropddown List", After that when we add session system the writer will be auto selected

--- MvcCamp/Controllers/HeadingController.cs
+++ MvcCamp/Controllers/HeadingController.cs
@@ -3,13 +3,15 @@ using DataAccesLayer.Concrete;
 using DataAccesLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MvcCamp.Controllers
 {
     public class HeadingController : Controller
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal(new Context()));
-
+        CategoryManager cm = new CategoryManager(new EfCategoryDal(new Context()));
+        WriterManager WriterManager = new WriterManager(new EfWriterDal(new Context()));
         public IActionResult Index()
         {
             var headingValues = hm.GetList();
@@ -18,7 +20,20 @@ namespace MvcCamp.Controllers
         [HttpGet]
         public IActionResult AddHeading()
         {
-            
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            List<SelectListItem> valueWriter = (from x in WriterManager.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurName,
+                                                    Value = x.WriterID.ToString()
+                                                }).ToList();
+            ViewBag.vlc = valueCategory;
+            ViewBag.vlw = valueWriter;
             return View();
         }
         [HttpPost]  
