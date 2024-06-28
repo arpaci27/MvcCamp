5ca856e81bd994363a97635b846328b864b93ee9
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-28 03:47:03 +0300

    Admin Login Form UI Added.

--- DataAccesLayer/Migrations/20240628003723_mig_add_adminClass.Designer.cs
+++ DataAccesLayer/Migrations/20240628003723_mig_add_adminClass.Designer.cs
@@ -4,6 +4,7 @@ using DataAccesLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccesLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240628003723_mig_add_adminClass")]
+    partial class mig_add_adminClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
