80bcb189ae1a02736c88102e932d9318f65121eb
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-04 02:30:21 +0300

    Listing of All Headings in the Showcase

--- MvcCamp/Controllers/DefaultController.cs
+++ MvcCamp/Controllers/DefaultController.cs
@@ -2,20 +2,28 @@
 using DataAccesLayer.Concrete;
 using DataAccesLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MvcCamp.Controllers
 {
+    [AllowAnonymous]
     public class DefaultController : Controller
     {
-        HeadingManager headingManeger = new HeadingManager(new EfHeadingDal(new Context()));
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal(new Context()));
+        ContentManager contentManager = new ContentManager(new EfContentDal(new Context()));
+
         public IActionResult Headings()
         {
-            var headingList = headingManeger.GetList();
-           return View(headingList);
+            var headingList = headingManager.GetList();
+            ViewBag.ContentList = contentManager.GetList(); // Store contentList in ViewBag
+            return View(headingList);
         }
-        public IActionResult Index()
+
+        public PartialViewResult Index()
         {
-            return View();
+            var contentList = contentManager.GetList();
+            return PartialView(contentList);
         }
     }
 }
