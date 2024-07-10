5f140ca7d5c9fb132b49561e17febaef1339989b
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-10 03:04:27 +0300

    Google Charts Added.

---
+++ MvcCamp/Controllers/ChartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCamp.Models;
+using System.Collections.Generic;
+
+namespace MvcCamp.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CategoryChart()
+        {
+            return Json(BlogList());
+        }
+
+        private List<CategoryClass> BlogList()
+        {
+            var categoryClass = new List<CategoryClass>
+            {
+                new CategoryClass { CategoryName = "Yazılım", CategoryCount = 8 },
+                new CategoryClass { CategoryName = "Seyahat", CategoryCount = 4 },
+                new CategoryClass { CategoryName = "Teknoloji", CategoryCount = 6 }
+            };
+            return categoryClass;
+        }
+    }
+}
--- MvcCamp/Models/ErrorViewModel.cs
+++ MvcCamp/Models/ErrorViewModel.cs
@@ -1,9 +1,6 @@
-namespace MvcCamp.Models
+﻿namespace MvcCamp.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
